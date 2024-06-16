1ba366621e65f249c2d88a3ca79f613afedf8ea6
OmJavia <omjavia18@gmail.com>
2024-06-16 16:20:03 +0530

    First Commit

---
+++ qrcode simple.py
@@ -0,0 +1,12 @@
+# import qrcode
+# features = qrcode.QRCode(version=1, box_size=20, border = 0)
+# features.add_data("https://drive.google.com/uc?export=download&id=1rvRDgSp8flH74LsiUg1m_LpyWuxwWcXA")
+# features.make(fit=True)
+# Generate_Image = features.make_image(fill_color = "Black", back_colour="White")
+# Generate_Image.save("D3.png")
+
+
+import qrcode
+img =  qrcode.make("https://drive.google.com/uc?export=download&id=1rvRDgSp8flH74LsiUg1m_LpyWuxwWcXA")
+img.save('Resume1.png')
+img.show("Resume1.png")
